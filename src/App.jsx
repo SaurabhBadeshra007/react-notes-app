90f84800723f08adc72344acbc5cd77cc083bdb3
Saurabh Badeshra <156070026+SaurabhBadeshra007@users.noreply.github.com>
2026-06-29 21:31:41 +0530

    Initial React project setup with Vite

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen bg-black text-white'>
+      <form className='flex p-10  ' >
+    <input type="text" placeholder='Enter Notes Heading'
+    className='px-5 py-2 border-2 rounded ' />
+    <input name="" placeholder="Enter details" id=""/>
+
+      </form>
+    </div>
+  )
+}
+
+export default App
